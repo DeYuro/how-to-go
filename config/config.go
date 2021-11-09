94a6c2f946df0d8ace76a91b208f78631fb8dc2b
Denis Shashkov <dshashkov@beget.ru>
2021-11-09 23:55:20 +0300

    load config

--- config/config.go
+++ config/config.go
@@ -1,7 +1,66 @@
 package main
 
-func main()  {
-	
+import (
+	"context"
+	"fmt"
+	"github.com/jinzhu/configor"
+	"github.com/k0kubun/pp"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Foo Foo `yaml:"foo"`
+	Bar Bar `yaml:"bar"`
 }
 
+type Foo struct {
+	Text     string        `yaml:"text"`
+	Numbers  int           `yaml:"numbers"`
+	Interval time.Duration `yaml:"interval"`
+}
 
+type Bar struct {
+	Foo *Foo `yaml:"foo"`
+}
+
+func main() {
+	if err := app(); err != nil {
+		log.WithError(err).Fatal("application failed with error")
+	}
+}
+
+func app() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	errCh := make(chan error)
+
+	go func() {
+		errCh <- start(cancel)
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Info("Service shutdown by ctx.Done")
+		return nil
+	case err := <-errCh:
+		return err
+	}
+}
+
+func start(cancel context.CancelFunc) error {
+	pp.Println(loadConfig("./config.yml"))
+	return nil
+}
+
+func loadConfig(pathToConfig string) *Config {
+	config := new(Config)
+
+	err := configor.Load(config, pathToConfig)
+	if err != nil {
+		fmt.Println("Failed to load configuration file " + err.Error())
+		os.Exit(1)
+	}
+
+	return config
+}
--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.7.0
 	github.com/go-redis/redis/v8 v8.11.1
 	github.com/hashicorp/consul/api v1.11.0
+	github.com/jinzhu/configor v1.2.1
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
 	github.com/mailru/go-clickhouse v1.6.0
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/grpc v1.39.0
