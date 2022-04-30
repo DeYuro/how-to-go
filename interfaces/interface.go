6518a856e1ccf488156554a777433dddde0e761c
Denis Shashkov <dshashkov@beget.ru>
2022-04-30 21:22:23 +0300

    examples + code

--- interfaces/interface.go
+++ interfaces/interface.go
@@ -1,6 +1,10 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 type FighterType string
 
@@ -11,9 +15,12 @@ const (
 
 type Fighter interface {
 	getHp() int
-	RestoreHp()
+	restoreHp()
+	takeDamage(damage int)
+	void()
 }
 
+//Human value type receiver methods
 type Human struct {
 	Hp          int
 	MaxHp       int
@@ -24,7 +31,7 @@ func (h Human) getHp() int {
 	return h.Hp
 }
 
-func (h Human) RestoreHp() {
+func (h Human) restoreHp() {
 	if h.MaxHp < (h.Hp + h.RestoreStep) {
 		h.Hp += h.RestoreStep
 	}
@@ -32,6 +39,20 @@ func (h Human) RestoreHp() {
 	h.Hp = h.MaxHp
 }
 
+func (h Human) takeDamage(damage int) {
+	if (h.Hp - damage) < 0 {
+		h.Hp = 0
+	}
+
+	h.Hp -= damage
+}
+
+// void pointer receiver invalid for human{....}  but valid for &human{...} other methods valid also
+func (h *Human) void() {
+
+}
+
+// AI pointer type receiver methods
 type AI struct {
 	Hp    int
 	MaxHp int
@@ -41,14 +62,26 @@ func (a *AI) getHp() int {
 	return a.Hp
 }
 
-func (a *AI) RestoreHp() {
-	a.Hp = a.MaxHp
+func (a *AI) restoreHp() {
+	a.Hp = a.MaxHp - 1
 }
 
+func (a *AI) takeDamage(damage int) {
+	if (a.Hp - damage) < 0 {
+		a.Hp = 0
+	}
+
+	a.Hp -= damage
+}
+
+// void value receiver valid for &AI{...}
+func (a AI) void() {
+
+}
 func createFighter(fighterType FighterType, maxHp, restoreStep int) (Fighter, error) {
 	switch fighterType {
 	case human:
-		return Human{
+		return &Human{
 			Hp:          maxHp,
 			MaxHp:       maxHp,
 			RestoreStep: restoreStep,
@@ -61,4 +94,30 @@ func createFighter(fighterType FighterType, maxHp, restoreStep int) (Fighter, er
 	}
 
 	return nil, errors.New("wrong type")
+}
+
+func main() {
+	player, err := createFighter(human, 100, 10)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("Human(value receiver) created and got have: %d\n", player.getHp())       // 100
+	player.takeDamage(42)                                                                // pointless coz value receiver
+	fmt.Printf("Human(value receiver) take damage and have HP: %d\n", player.getHp())    // 100
+	player.restoreHp()                                                                   // pointless coz value receiver
+	fmt.Printf("Human(value receiver) restore health and have HP: %d\n", player.getHp()) // 100
+
+	bot, err := createFighter(ai, 1000, 10)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("AI(pointer receiver) created and got have: %d\n", bot.getHp()) //1000
+	bot.takeDamage(444)
+	fmt.Printf("AI(pointer receiver) take damage and have HP: %d\n", bot.getHp()) //556
+	bot.restoreHp()
+	fmt.Printf("AI(pointer receiver) restore health and have HP: %d\n", bot.getHp()) //999
 }
